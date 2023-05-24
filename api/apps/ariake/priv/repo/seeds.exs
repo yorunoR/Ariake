80220d3c4254acd19b5f113f6ea60eeedf396729
Yoshioka <sss.yoshioka@gmail.com>
2023-05-24 14:23:29 +0000

    🎸 feat: set built-in prompts

--- api/apps/ariake/priv/repo/seeds.exs
+++ api/apps/ariake/priv/repo/seeds.exs
@@ -11,6 +11,40 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Ariake.Admin
+alias Ariake.Repo
+alias Schemas.Flow.Prompt
 
 email = System.get_env("ADMIN_USER_EMAIL")
 Admin.register_admin_user(%{email: email, password: "123456123456"})
+
+# Summarize
+summarize = """
+  Summarize the text delimited by triple backticks into a single {__summarize__lang} sentence.
+
+  ```{__summarize__text}```
+"""
+
+Repo.insert!(%Prompt{
+  template: summarize,
+  variables: %{
+    text_key: "__summarize__text",
+    lang_key: "__summarize__lang"
+  }
+})
+
+# ToJson
+to_json = """
+  Provide the text delimited by triple backticks into JSON format with the following keys:
+  {__to_json__keys}.
+
+
+  ```{__to_json__data}```
+"""
+
+Repo.insert!(%Prompt{
+  template: to_json,
+  variables: %{
+    data_key: "__to_json__data",
+    keys_key: "__to_json__keys"
+  }
+})
